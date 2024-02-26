ee822830bed6871721a7e65e4fdeaa43cf558adb
Kevin Miller <kevin@neomantra.net>
2024-02-26 09:24:43 -0600

    Add helper push,pop navigation tea.Cmd

--- navstack/messages.go
+++ navstack/messages.go
@@ -1,5 +1,10 @@
 package navstack
 
+import (
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/kevm/bubbleo/utils"
+)
+
 // ReloadCurrent is a message that can be sent to the menu model to reload the currently selected menu choice
 type ReloadCurrent struct{}
 
@@ -8,4 +13,12 @@ type PopNavigation struct{}
 
 type PushNavigation struct {
 	Item NavigationItem
+}
+
+func PopNavigationCmd() tea.Cmd {
+	return utils.Cmdize(PopNavigation{})
+}
+
+func PushNavigationCmd(item NavigationItem) tea.Cmd {
+	return utils.Cmdize(PushNavigation{Item: item})
 }
